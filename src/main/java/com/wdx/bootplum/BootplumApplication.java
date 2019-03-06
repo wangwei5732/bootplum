2423de36af35b07e7149c192cb9a5c4e694e4b0c
wangwei <1310692942@qq.com>
2019-03-06 22:04:26 +0800

    初始项目上传

---
+++ src/main/java/com/wdx/bootplum/BootplumApplication.java
@@ -0,0 +1,13 @@
+package com.wdx.bootplum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootplumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootplumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wdx/bootplum/BootplumApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wdx.bootplum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootplumApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
